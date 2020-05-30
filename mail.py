ee4316e7ab167d89c39bc6db51d3a911ddfbdf7d
Vivek Singare <vmsingare11@gmail.com>
2020-05-30 12:38:58 +0530

    Update mail.py

--- mail.py
+++ mail.py
@@ -1,12 +1,12 @@
 import smtplib
-rcvr = "saptarsiroy12@gmail.com"
-sender = "chattopadhyaysaranya@gmail.com"
-msg = "Hey Developer, your model has greater than 96% accuracy and is ready to use for predictions!!" 
-pswd = "***" #password for sender's mail address
+receiver = "vmsingare11@gmail.com"
+sender = "vimsingare11@gmail.com"
+text = "Hello Developer, your model achieved more than 96% accuracy and is ready to use for predictions!!!" 
+pass = "" #password for sender's mail address
 server = smtplib.SMTP('smtp.gmail.com',587)
 server.ehlo()
 server.starttls()
-server.login(sender,pswd)
+server.login(sender,pass)
 print("Access Granted")
-server.sendmail(sender,rcvr,msg)
+server.sendmail(sender,reciever,text)
 print("Mail sent!!")
